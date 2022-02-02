45213455a1420133b66488b1f1c9eed17ea07ced
Johannes Müller <38459088+jo-mueller@users.noreply.github.com>
2022-02-02 14:05:13 +0100

    cleaned up code

--- src/napari_stress/_tracing.py
+++ src/napari_stress/_tracing.py
@@ -17,7 +17,7 @@ def get_traces(image, start_pts, target_pts, **kwargs):
 
     Parameters
     ----------
-    image : array 
+    image : array
         3D-image containing the raw intesity values
     start_pts : Nx3 array
         list of length-3 coordinate vectors that refer to the starting location of each trace.
@@ -28,44 +28,44 @@ def get_traces(image, start_pts, target_pts, **kwargs):
     -------
     list: List of intensity profiles
     """
-    
+
     sample_distance = kwargs.get('sample_distance', 1)
     detection = kwargs.get('detection', 'quick_edge')
     fluorescence = kwargs.get('fluorescence', 'interior')
-    
+
     # Do type conversion if points come from pandas array
     if type(target_pts) == pd.core.series.Series:
         target_pts = np.vstack(target_pts)
     if type(start_pts) == pd.core.series.Series:
         start_pts = np.vstack(start_pts)
-    
+
     # If only a single start point is provided
     if target_pts.shape[0] == 3:
         target_pts = target_pts.transpose()
-    
+
     # allocate vector for origin points, repeat N times if only one coordinate is provided
     if len(start_pts.shape) == 1:
         start_pts = np.asarray([start_pts]).repeat(target_pts.shape[0], axis=0)
-        
+
     # Create coords for interpolator
     Z = np.arange(0, image.shape[0], 1)
     Y = np.arange(0, image.shape[1], 1)
     X = np.arange(0, image.shape[2], 1)
     coords = (Z, Y, X)
-    
+
     profiles = []
     surface_points = []
-    
+
     # Iterate over all provided target points
     for idx in tqdm.tqdm(range(target_pts.shape[0]), desc='Shooting rays: '):
-        
+
         profile = []
         r = 0
-        
+
         # calculate normalized vector from center to target
         v = target_pts[idx, :] - start_pts[idx, :]
         v  = v/np.linalg.norm(v)
-        
+
         while True:
 
             # walk from center outwards
@@ -74,21 +74,21 @@ def get_traces(image, start_pts, target_pts, **kwargs):
                 profile.append(interpolate.interpn(coords, image, p)[0])
             except Exception:
                 break
-                    
+
             r += sample_distance
-            
+
         if detection == 'quick_edge':
             if fluorescence == 'interior':
                 # Find point where gradient is maximal
                 idx_border = np.argmax(np.abs(np.diff(profile)))
-            
+
             elif fluorescence == 'surface':
                 # Find point where intensity is maximal
                 idx_border = np.argmax(profile)
-        
+
         # get coordinate of surface point
         surf_point = start_pts[idx] + (idx_border * sample_distance) * v
-        
+
         # Append to list
         surface_points.append(surf_point)
         profiles.append(profile)
@@ -96,19 +96,10 @@ def get_traces(image, start_pts, target_pts, **kwargs):
     surface_points = np.asarray(surface_points)
     if surface_points.shape[0] == 3:
         surface_points= surface_points.transpose()
-        
+
     if detection == 'quick_edge':
         errors = np.zeros(np.max(target_pts.shape))
         FitParams = np.zeros(np.max(target_pts.shape))
-        
-    # df = pd.DataFrame(columns=['XYZ', 'X', 'Y', 'Z', 'FitErrors', 'FitParams'])
-    
-    # points['XYZ'] = surface_points
-    # surface_points = np.asarray(surface_points)
-    # # points['X'] = surface_points[:, 0]
-    # # points['Y'] = surface_points[:, 1]
-    # # points['Z'] = surface_points[:, 2]
-    # points['FitErrors'] = errors
-    # points['FitParams'] = FitParams
-        
+
+
     return surface_points, errors, FitParams
