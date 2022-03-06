9bd6c71f5fcbfbd6ee94b085c022fdba4a9e90aa
Johannes Müller <38459088+jo-mueller@users.noreply.github.com>
2022-03-06 13:27:24 +0100

    merged functions fit_ellipse and fit_ellipse_4d into single function

--- src/napari_stress/_preprocess.py
+++ src/napari_stress/_preprocess.py
@@ -5,17 +5,16 @@ Created on Mon Oct 18 17:18:26 2021
 @author: johan
 """
 
-import tifffile as tf
-
 from skimage.transform import rescale
 from ._surface import fibonacci_sphere
 from skimage import filters, measure
-from scipy import ndimage
 
 import pandas as pd
 import numpy as np
-import os
+import vedo
+
 import tqdm
+
 
 def preprocessing(image: np.ndarray,
                   vsx: float,
@@ -60,12 +59,10 @@ def preprocessing(image: np.ndarray,
     return image_resampled, mask_resampled
 
 
-
 def fit_ellipse_4D(binary_image: np.ndarray,
                    n_rays: np.uint16):
 
     n_frames = binary_image.shape[0]
-    print(binary_image.shape)
 
     # Fit ellipse
     pts = np.zeros([n_rays * n_frames, 4])
@@ -77,7 +74,7 @@ def fit_ellipse_4D(binary_image: np.ndarray,
     return pts
 
 def fit_ellipse(binary_image: np.ndarray,
-                n_samples: np.uint16 = 256) -> np.ndarray:
+                n_samples: np.uint16 = 256) -> list:
     """
     Fit an ellipse to a binary image.
 
@@ -94,42 +91,53 @@ def fit_ellipse(binary_image: np.ndarray,
         DESCRIPTION.
 
     """
+    if not len(binary_image.shape) == 4:
+        binary_image = binary_image[None, :]
+    
+    # Allocate points
+    n_frames = binary_image.shape[0]
+    pts = []
 
-    props = measure.regionprops_table(binary_image, properties=(['centroid']))
-    CoM = [props['centroid-0'][0],
-           props['centroid-1'][0],
-           props['centroid-2'][0]]
+    for t in range(n_frames):
 
-    # Create coordinate grid:
-    ZZ, YY, XX = np.meshgrid(np.arange(binary_image.shape[0]),
-                              np.arange(binary_image.shape[1]),
-                              np.arange(binary_image.shape[2]), indexing='ij')
+        _binary_image = binary_image[t]
+        props = measure.regionprops_table(_binary_image,
+                                          properties=(['centroid']))
+        CoM = [props['centroid-0'][0],
+               props['centroid-1'][0],
+               props['centroid-2'][0]]
+    
+        # Create coordinate grid:
+        ZZ, YY, XX = np.meshgrid(np.arange(_binary_image.shape[0]),
+                                  np.arange(_binary_image.shape[1]),
+                                  np.arange(_binary_image.shape[2]), indexing='ij')
+    
+        # Substract center of mass and mask
+        ZZ = (ZZ.astype(float) - CoM[0]) * _binary_image
+        YY = (YY.astype(float) - CoM[1]) * _binary_image
+        XX = (XX.astype(float) - CoM[2]) * _binary_image
+    
+        # Concatenate to single (Nx3) coordinate vector
+        ZYX = np.vstack([ZZ.ravel(), YY.ravel(), XX.ravel()]).transpose((1, 0))
+    
+        # Calculate orientation matrix
+        S = 1/np.sum(_binary_image) * np.dot(ZYX.conjugate().T, ZYX)
+        D, R = np.linalg.eig(S)
+    
+        # # get angles from rotation matrix: http://planning.cs.uiuc.edu/node103.html
+        # alpha = np.arctan(R[1,0]/R[0,0])/np.pi*180
+        # beta = np.arctan(-R[2, 0]/np.sqrt(R[2,1]**2 + R[2,2]**2))/np.pi*180
+        # gamma = np.arctan(R[2,1]/R[2,2])/np.pi*180
+    
+        # Now create points on the surface of an ellipse
+        props = pd.DataFrame(measure.regionprops_table(_binary_image, properties=(['major_axis_length', 'minor_axis_length'])))
+        semiAxesLengths = [props.loc[0].minor_axis_length/2,
+                           props.loc[0].major_axis_length/2,
+                           props.loc[0].major_axis_length/2]
+        _pts = fibonacci_sphere(semiAxesLengths, R.T, CoM, samples=n_samples)
+        pts.append(vedo.pointcloud.Points(_pts))
 
-    # Substract center of mass and mask
-    ZZ = (ZZ.astype(float) - CoM[0]) * binary_image
-    YY = (YY.astype(float) - CoM[1]) * binary_image
-    XX = (XX.astype(float) - CoM[2]) * binary_image
-
-    # Concatenate to single (Nx3) coordinate vector
-    ZYX = np.vstack([ZZ.ravel(), YY.ravel(), XX.ravel()]).transpose((1, 0))
-
-    # Calculate orientation matrix
-    S = 1/np.sum(binary_image) * np.dot(ZYX.conjugate().T, ZYX)
-    D, R = np.linalg.eig(S)
-
-    # get angles from rotation matrix: http://planning.cs.uiuc.edu/node103.html
-    alpha = np.arctan(R[1,0]/R[0,0])/np.pi*180
-    beta = np.arctan(-R[2, 0]/np.sqrt(R[2,1]**2 + R[2,2]**2))/np.pi*180
-    gamma = np.arctan(R[2,1]/R[2,2])/np.pi*180
-
-    # Now create points on the surface of an ellipse
-    props = pd.DataFrame(measure.regionprops_table(binary_image, properties=(['major_axis_length', 'minor_axis_length'])))
-    semiAxesLengths = [props.loc[0].major_axis_length/2,
-                       props.loc[0].minor_axis_length/2,
-                       props.loc[0].minor_axis_length/2]
-    pts = fibonacci_sphere(semiAxesLengths, R.T, CoM, samples=n_samples)
-
-    return np.asarray(pts)
+    return pts
 
 def resample(image, vsx, vsy, vsz, res_mode='high'):
     """
