859497278986cec880c2df176d61394ddc0b1106
Johannes Müller <38459088+jo-mueller@users.noreply.github.com>
2022-03-30 19:53:10 +0200

    Merged two tracing function to single

--- src/napari_stress/_refine_surfaces.py
+++ src/napari_stress/_refine_surfaces.py
@@ -4,7 +4,8 @@ import vedo
 
 from napari_tools_menu import register_function
 
-from napari.types import SurfaceData, ImageData, LayerDataTuple
+from napari.types import SurfaceData, LayerDataTuple, ImageData
+
 from scipy.interpolate import RegularGridInterpolator
 from scipy.optimize import curve_fit
 import numpy as np
@@ -19,57 +20,19 @@ class fit_methods(Enum):
     fancy_edge_fit = 1
 
 class fluorescence_types(Enum):
-    interior = 0
-    surface = 1
+    interior = {'mode': 0, 'n_params': 4}
+    surface = {'mode': 1, 'n_params': 3}
 
 @register_function(menu="Surfaces > Retrace surface vertices (vedo, nppas)")
 def trace_refinement_of_surface(image: ImageData,
                                 surface: SurfaceData,
                                 trace_length: float = 2.0,
                                 sampling_distance: float = 0.1,
-                                fit_method: fit_methods = fit_methods.quick_edge_fit,
-                                fluorescence: fluorescence_types = fluorescence_types.interior
+                                fit_method: fit_methods = fit_methods.fancy_edge_fit,
+                                fluorescence: fluorescence_types = fluorescence_types.interior,
+                                scale: np.ndarray = np.array([1.0, 1.0, 1.0]),
+                                show_progress: bool = True
                                 )-> List[LayerDataTuple]:
-
-    # parse inputs
-    if isinstance(fit_method, int):
-        pass
-    else:
-        fit_method = fit_method.value
-
-    if isinstance(fluorescence, int):
-        pass
-    else:
-        fluorescence = fluorescence.value
-
-    # Convert to mesh and calculate normals
-    mesh = vedo.mesh.Mesh((surface[0], surface[1]))
-    mesh.computeNormals()
-
-    # Define start and end points for the surface tracing vectors
-    trace_length = np.repeat(trace_length, mesh.N())
-    start_points = mesh.points() - (1/2)*trace_length[:, None] * mesh.pointdata['Normals']
-    target_points = mesh.points() + (1/2)*trace_length[:, None] * mesh.pointdata['Normals']
-
-    new_points, properties = _get_traces(image,
-                                         start_points,
-                                         target_points,
-                                         sampling_distance=sampling_distance,
-                                         fit_method=fit_method,
-                                         fluorescence=fluorescence)
-    surf = ((new_points,  properties['fit_errors']),
-            {'colormap': 'magma'},
-            'Points')
-    vector_data = np.array([start_points, target_points - start_points])
-    vectors = (vector_data.transpose((1,0,2)), {}, 'Vectors')
-    return [surf, vectors]
-
-def _get_traces(image: np.ndarray,
-               start_pts: np.ndarray,
-               target_pts: np.ndarray,
-               sampling_distance: float =  0.1,
-               fit_method: int = 0,
-               fluorescence: int = 0) -> [np.ndarray, dict]:
     """
     Generate intensity profiles along traces.
 
@@ -103,43 +66,65 @@ def _get_traces(image: np.ndarray,
     FitParams : np.ndarray
         Nx3 array with determined fit parameters if detection was set to `advanced`
     """
-    # allocate vector for origin points, repeat N times if only one coordinate is provided
-    if len(start_pts.shape) == 1:
-        start_pts = np.asarray([start_pts]).repeat(target_pts.shape[0], axis=0)
 
-    # Calculate trace lengths for given start/target points and calculate
-    # number of sampling points given the passed sampling rate
-    vectors = target_pts - start_pts
+    # parse inputs
+    if isinstance(fit_method, int):
+        fit_method = fit_methods(fit_method)
 
-    trace_lengths = np.linalg.norm(vectors, axis=1)
-    n_samples = np.asarray(trace_lengths/sampling_distance, dtype=int)
-    v_step = vectors/n_samples[:, None]
+    if isinstance(fluorescence, int):
+        fluorescence = fluorescence_types(fluorescence)
+
+    # Convert to mesh and calculate normals
+    mesh = vedo.mesh.Mesh((surface[0], surface[1]))
+    mesh.computeNormals()
+
+    # Define start and end points for the surface tracing vectors
+    n_samples = int(trace_length/sampling_distance)
+    start_pts = mesh.points()/scale[None, :] - 0.5 * trace_length * mesh.pointdata['Normals']
+
+    # Define trace vectors for full length and for single step
+    vectors = trace_length * mesh.pointdata['Normals']
+    v_step = vectors/n_samples
 
     # Create coords for interpolator
     X1 = np.arange(0, image.shape[0], 1)
     X2 = np.arange(0, image.shape[1], 1)
     X3 = np.arange(0, image.shape[2], 1)
-    rgi = RegularGridInterpolator((X1, X2, X3), image)
+    rgi = RegularGridInterpolator((X1, X2, X3), image, bounds_error=False,
+                                  fill_value=image.min())
 
     # Allocate arrays for results
-    surface_points = np.zeros((len(start_pts), 3))
-    idx_of_border = np.zeros(len(start_pts))
+    surface_points = np.zeros_like(surface[0])
+    idx_of_border = np.zeros(mesh.N())
     fit_errors = []
     fit_params = []
     profiles = []
 
-    # Iterate over all provided target points
-    for idx in tqdm.tqdm(range(target_pts.shape[0])):
 
-        profile_coords = [start_pts[idx] + k * v_step[idx] for k in range(n_samples[idx])]
+    if show_progress:
+        tk = tqdm.tqdm(range(mesh.N(), desc = 'Processing vertices...'))
+    else:
+        tk = range(mesh.N())
+
+    # Iterate over all provided target points
+    for idx in tk:
+
+        profile_coords = [start_pts[idx] + k * v_step[idx] for k in range(n_samples)]
         profiles.append(rgi(profile_coords))
 
-        if fit_method == 0:
-            _idx_of_border = _quick_edge_fit(profiles[idx], mode=fluorescence)
+        if fit_method == fit_methods.quick_edge_fit:
+            _idx_of_border = _quick_edge_fit(profiles[idx],
+                                             mode=fluorescence.value['mode'])
             perror = np.array(0)
             popt = np.array(0)
-        elif fit_method == 1:
-            popt, perror = _fancy_edge_fit(profiles[idx], mode=fluorescence)
+        elif fit_method == fit_methods.fancy_edge_fit:
+            try:
+                popt, perror = _fancy_edge_fit(profiles[idx],
+                                               mode=fluorescence.value['mode'])
+            except:
+                popt = np.repeat(np.nan, fluorescence.value['n_params'])
+                perror = np.repeat(np.nan, fluorescence.value['n_params'])
+
             _idx_of_border = popt[0]
 
         idx_of_border[idx] = _idx_of_border
@@ -147,40 +132,62 @@ def _get_traces(image: np.ndarray,
         fit_params.append(popt)
 
     # convert to arrays
-    fit_errors = np.asarray(fit_errors)
-    fit_params = np.asarray(fit_params)
-    params_matrix = np.concatenate([fit_errors, fit_params], axis=1)
-    df = pd.DataFrame(params_matrix, columns=[''])
+    fit_errors = np.vstack(fit_errors)
+    fit_params = np.vstack(fit_params)
 
-    # Filter bad points
-    good_pts = _remove_outliers_by_index(df)
+    # Filter points to remove points with high fit errors
+    good_pts = _remove_outliers_by_index(pd.DataFrame(fit_errors))
+
     start_pts = start_pts[good_pts]
     idx_of_border = idx_of_border[good_pts]
     v_step = v_step[good_pts]
     fit_params = fit_params[good_pts, :]
     fit_errors = fit_errors[good_pts, :]
-    profiles = profiles[good_pts]
+    profiles = np.array(profiles)[good_pts]
 
-    # get coordinate of surface point and errors
-    surface_points = start_pts + idx_of_border[:, None] * v_step
+    # get coordinate of surface point and errors (respect scale for this)
+    surface_points = (start_pts + idx_of_border[:, None] * v_step) * scale[None, :]
+    vectors = np.array([surface_points, idx_of_border[:, None] * v_step]).transpose((1, 0, 2))
 
-    return surface_points, {'fit_errors': fit_errors,
-                            'fit_params': fit_params,
-                            'profiles': profiles}
+    #TODO
+    # Turn relevant parameters into single dataframe to attach to returned points layer
+    # A viewer on such properties can then visualize this
+
+    properties = {'fit_errors': fit_errors, 'fit_params': fit_params, 'profiles': profiles}
+
+    return [(surface_points, {'properties': properties}, 'Points'),
+            (vectors, {'name': 'Trace vectors'}, 'Vectors')]
 
 def _remove_outliers_by_index(df):
+    "Filter all rows that qualify as outliers based on column-statistics."
     indices = np.ones(len(df), dtype=bool)
+    df = df.dropna()
     for col in df.columns:
         Q1 = df[col].quantile(0.25)
         Q3 = df[col].quantile(0.75)
         IQR = Q3 - Q1
-        indices[(df[col] < (Q1 - 1.5 * IQR)) |(df[col] > (Q3 + 1.5 * IQR))] = False
+        indices[df[df[col] > (Q3 + 1.5 * IQR)].index] = False
     return indices
 
 
 def _fancy_edge_fit(profile: np.ndarray, mode: int = 0) -> float:
-    "https://docs.scipy.org/doc/scipy/reference/generated/scipy.optimize.curve_fit.html"
+    """
+    Fit a line profile with a gaussian normal curve or a sigmoidal function.
+    https://docs.scipy.org/doc/scipy/reference/generated/scipy.optimize.curve_fit.html
 
+    Parameters
+    ----------
+    profile : np.ndarray
+        DESCRIPTION.
+    mode : int, optional
+        DESCRIPTION. The default is 0.
+
+    Returns
+    -------
+    float
+        DESCRIPTION.
+
+    """
     if mode == 0:
 
         # Make sure that intensity goes up along ray so that fit can work
@@ -205,9 +212,9 @@ def _quick_edge_fit(profile: np.ndarray, mode = 0) -> int:
     elif mode == 1:
         return np.argmax(profile)
 
-def _sigmoid(x, x0, L, k, b):
+def _sigmoid(x, center, amplitude, slope, offset):
     "https://stackoverflow.com/questions/55725139/fit-sigmoid-function-s-shape-curve-to-data-using-python"
-    return L / (1 + np.exp(-k*(x-x0))) + b
+    return amplitude / (1 + np.exp(-slope*(x-center))) + offset
 
-def _gaussian(x, x0, sigma, A):
-    return A/np.sqrt((2*np.pi*sigma**2)) * np.exp(-(x - x0)**2 / (2*sigma**2))
+def _gaussian(x, center, sigma, amplitude):
+    return amplitude/np.sqrt((2*np.pi*sigma**2)) * np.exp(-(x - center)**2 / (2*sigma**2))
