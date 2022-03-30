139f309cb3c9866b371d4b9424f03a561863d1fa
Johannes Müller <38459088+jo-mueller@users.noreply.github.com>
2022-03-30 19:52:26 +0200

    Add newq/changed function to init

--- src/napari_stress/__init__.py
+++ src/napari_stress/__init__.py
@@ -1,11 +1,14 @@
 
 __version__ = "0.0.1"
 
-from ._stress import stress_widget
+from ._refine_surfaces import trace_refinement_of_surface
+from ._preprocess import resample
+from ._surface import surface_from_label, list_of_surfaces_to_surface
+from ._utils import list_of_points_to_pointsdata
 from napari_plugin_engine import napari_hook_implementation
 
 @napari_hook_implementation
 def napari_experimental_provide_dock_widget():
     return [
-        stress_widget
+        trace_refinement_of_surface, resample
     ]
