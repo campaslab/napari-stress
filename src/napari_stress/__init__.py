fda5d8f17fe5abcb189ac1d98fc3fb0c52a53658
Johannes Müller <38459088+jo-mueller@users.noreply.github.com>
2022-04-07 17:22:53 +0200

    added new functions to init

--- src/napari_stress/__init__.py
+++ src/napari_stress/__init__.py
@@ -3,8 +3,8 @@ __version__ = "0.0.1"
 
 from ._refine_surfaces import trace_refinement_of_surface
 from ._preprocess import resample
-from ._surface import surface_from_label, list_of_surfaces_to_surface, adjust_surface_density
-from ._utils import list_of_points_to_pointsdata
+from ._surface import surface_from_label, adjust_surface_density, smooth_sinc, smoothMLS2D
+from ._utils import list_of_points_to_pointsdata, list_of_surfaces_to_surface, list_of_points_to_points, points_to_list_of_points
 from napari_plugin_engine import napari_hook_implementation
 
 @napari_hook_implementation
