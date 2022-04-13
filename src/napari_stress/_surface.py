1d95b61b8b95db88fe0d7d3b47617ed48d685c75
Johannes Müller <38459088+jo-mueller@users.noreply.github.com>
2022-04-13 16:56:53 +0200

    Added frame_by_frame decorator to all the functions

--- src/napari_stress/_surface.py
+++ src/napari_stress/_surface.py
@@ -3,33 +3,59 @@
 import numpy as np
 import napari_process_points_and_surfaces as nppas
 from napari.types import LabelsData, SurfaceData, PointsData
-from napari.layers import Points, Surface
-# from ._utils import frame_by_frame
+from ._utils import frame_by_frame
 
 import vedo
 import typing
 
 
+@frame_by_frame
 def reconstruct_surface(points: PointsData,
                         radius: float = None,
                         sampleSize: int = None,
-                        holeFilling: bool = True,
-                        bounds: tuple = (),
-                        padding: float = 0.05
-                        ) -> Surface:
-    pointcloud = vedo.pointcloud.Points(points)
+                        holeFilling: bool = True) -> SurfaceData:
+    """
+    Reconstruct a surface from a set of points.
 
-    dims = np.max(pointcloud.points(), axis=0) - np.min(pointcloud.points(), axis=0)
-    surf = pointcloud.reconstructSurface(dims=dims.astype(int),
-                                         radius=radius,
+    Parameters
+    ----------
+    points : PointsData (napari.types.PointsData)
+
+    Returns
+    -------
+    SurfaceData: napari.types.SurfaceData
+
+    """
+    # Catch magicgui default values
+    if radius == 0:
+        radius = None
+
+    if sampleSize == 0:
+        sampleSize = None
+
+    pointcloud = vedo.pointcloud.Points(points)
+    surf = pointcloud.reconstructSurface(radius=radius,
                                          sampleSize=sampleSize,
-                                         holeFilling=holeFilling,
-                                         bounds=bounds,
-                                         padding=padding)
+                                         holeFilling=holeFilling)
 
     return (surf.points(), np.asarray(surf.faces(), dtype=int))
 
+@frame_by_frame
+def smooth_laplacian(surface: SurfaceData,
+                     niter: int = 15,
+                     relax_factor: float = 0.1,
+                     edge_angle: float = 15,
+                     feature_angle: float = 60,
+                     boundary: bool = False) -> SurfaceData:
+    mesh = vedo.mesh.Mesh((surface[0], surface[1]))
+    mesh.smoothLaplacian(niter=niter, relaxfact=relax_factor,
+                         edgeAngle=edge_angle,
+                         featureAngle=feature_angle,
+                         boundary=boundary)
 
+    return (mesh.points(), np.asarray(mesh.faces()))
+
+@frame_by_frame
 def smooth_sinc(surface: SurfaceData,
                 niter: int = 15,
                 passBand: float = 0.1,
@@ -43,6 +69,7 @@ def smooth_sinc(surface: SurfaceData,
                 boundary=boundary)
     return (mesh.points(), np.asarray(mesh.faces(), dtype=int))
 
+@frame_by_frame
 def smoothMLS2D(points: PointsData,
                 f: float = 0.2,
                 radius: float = None) -> PointsData:
@@ -52,31 +79,27 @@ def smoothMLS2D(points: PointsData,
 
     return pointcloud.points()[pointcloud.info['isvalid']]
 
+@frame_by_frame
 def surface_from_label(label_image: LabelsData,
-                       scale: typing.Union[list, np.ndarray]) -> SurfaceData:
+                       scale: typing.Union[np.ndarray, list] = np.array([1.0, 1.0, 1.0])
+                       ) -> SurfaceData:
 
-    if isinstance(scale, list):
-        scale = np.array(scale)
+    surf = list(nppas.label_to_surface(label_image))
+    surf[0] = surf[0] * np.asarray(scale)[None, :]
 
-    n_frames = label_image.shape[0]
+    return surf
 
-    surfs = []
-    for t in range(n_frames):
-        surf = nppas.label_to_surface(label_image[t])
-        surfs.append(vedo.mesh.Mesh((surf[0] * scale[None, :], surf[1])))
-
-    return surfs
-
-
+@frame_by_frame
 def adjust_surface_density(surface: SurfaceData,
-                           density_target: float) -> vedo.mesh.Mesh:
+                           density_target: float) -> SurfaceData:
+    """Adjust the number of vertices of a surface to a defined density"""
 
     mesh = vedo.mesh.Mesh((surface[0], surface[1]))
     n_vertices_target = int(mesh.area() * density_target)
 
-    while mesh.N() < n_vertices_target:
-        mesh.subdivide()
+    while surface[0].shape[0] < n_vertices_target:
+        surface = nppas.subdivide_loop(surface)
 
-    mesh.decimate(N=n_vertices_target)
+    mesh = vedo.mesh.Mesh((surface[0], surface[1])).decimate(N=n_vertices_target)
 
-    return (mesh.points(), np.asarray(mesh.faces(), dtype=int))
+    return (mesh.points(), np.asarray(mesh.faces()))
