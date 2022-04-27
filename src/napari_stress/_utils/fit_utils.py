3a0c12a75be3cb5acb9eb28cd52e09313ab417f8
Johannes Müller <38459088+jo-mueller@users.noreply.github.com>
2022-04-27 23:05:49 +0200

    Moved fit functions to utility submodule

---
+++ src/napari_stress/_utils/fit_utils.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+import inspect
+
+def _sigmoid(x, center, amplitude, slope, offset):
+    "https://stackoverflow.com/questions/55725139/fit-sigmoid-function-s-shape-curve-to-data-using-python"
+    return amplitude / (1 + np.exp(-slope*(x-center))) + offset
+
+def _gaussian(x, center, sigma, amplitude):
+    return amplitude/np.sqrt((2*np.pi*sigma**2)) * np.exp(-(x - center)**2 / (2*sigma**2))
+
+def _detect_maxima(profile, center: float = None):
+    return np.argmax(profile)
+
+def _detect_drop(profile, center: float = None):
+    return np.argmax(np.abs(np.diff(profile)))
+
+def _func_args_to_list(func: callable) -> list:
+
+    sig = inspect.signature(func)
+    return list(sig.parameters.keys())
