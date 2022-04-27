cd2fce396c7f768f11bdf2915c98d3b01a0157e2
Johannes Müller <38459088+jo-mueller@users.noreply.github.com>
2022-04-27 22:00:50 +0200

    commented out ellipse fitting

--- src/napari_stress/_preprocess.py
+++ src/napari_stress/_preprocess.py
@@ -16,80 +16,80 @@ def resample(image: ImageData,
     return transform.rescale(image, scale=scale_factors)
 
 
-def fit_ellipse_4D(binary_image: np.ndarray,
-                   n_rays: np.uint16):
+# def fit_ellipse_4D(binary_image: np.ndarray,
+#                    n_rays: np.uint16):
 
-    n_frames = binary_image.shape[0]
+#     n_frames = binary_image.shape[0]
 
-    # Fit ellipse
-    pts = np.zeros([n_rays * n_frames, 4])
-    for t in range(n_frames):
-        _pts = fit_ellipse(binary_image=binary_image[t])
-        pts[t * n_rays : (t + 1) * n_rays, 1:] = _pts
-        pts[t * n_rays : (t + 1) * n_rays, 0] = t
+#     # Fit ellipse
+#     pts = np.zeros([n_rays * n_frames, 4])
+#     for t in range(n_frames):
+#         _pts = fit_ellipse(binary_image=binary_image[t])
+#         pts[t * n_rays : (t + 1) * n_rays, 1:] = _pts
+#         pts[t * n_rays : (t + 1) * n_rays, 0] = t
 
-    return pts
+#     return pts
 
-def fit_ellipse(binary_image: np.ndarray,
-                n_samples: np.uint16 = 256) -> list:
-    """
-    Fit an ellipse to a binary image.
+# def fit_ellipse(binary_image: np.ndarray,
+#                 n_samples: np.uint16 = 256) -> list:
+#     """
+#     Fit an ellipse to a binary image.
 
-    Parameters
-    ----------
-    binary_image : np.ndarray
-        DESCRIPTION.
-    n_samples : np.uint16, optional
-        DESCRIPTION. The default is 256.
+#     Parameters
+#     ----------
+#     binary_image : np.ndarray
+#         DESCRIPTION.
+#     n_samples : np.uint16, optional
+#         DESCRIPTION. The default is 256.
 
-    Returns
-    -------
-    TYPE
-        DESCRIPTION.
+#     Returns
+#     -------
+#     TYPE
+#         DESCRIPTION.
 
-    """
-    if not len(binary_image.shape) == 4:
-        binary_image = binary_image[None, :]
+#     """
+#     if not len(binary_image.shape) == 4:
+#         binary_image = binary_image[None, :]
 
-    # Allocate points
-    n_frames = binary_image.shape[0]
-    pts = []
+#     # Allocate points
+#     n_frames = binary_image.shape[0]
+#     pts = []
 
-    for t in range(n_frames):
+#     for t in range(n_frames):
 
-        _binary_image = binary_image[t]
-        props = measure.regionprops_table(_binary_image,
-                                          properties=(['centroid']))
-        CoM = [props['centroid-0'][0],
-               props['centroid-1'][0],
-               props['centroid-2'][0]]
+#         _binary_image = binary_image[t]
+#         props = measure.regionprops_table(_binary_image,
+#                                           properties=(['centroid']))
+#         CoM = [props['centroid-0'][0],
+#                props['centroid-1'][0],
+#                props['centroid-2'][0]]
 
-        # Create coordinate grid:
-        ZZ, YY, XX = np.meshgrid(np.arange(_binary_image.shape[0]),
-                                  np.arange(_binary_image.shape[1]),
-                                  np.arange(_binary_image.shape[2]), indexing='ij')
+#         # Create coordinate grid:
+#         ZZ, YY, XX = np.meshgrid(np.arange(_binary_image.shape[0]),
+#                                   np.arange(_binary_image.shape[1]),
+#                                   np.arange(_binary_image.shape[2]), indexing='ij')
 
-        # Substract center of mass and mask
-        ZZ = (ZZ.astype(float) - CoM[0]) * _binary_image
-        YY = (YY.astype(float) - CoM[1]) * _binary_image
-        XX = (XX.astype(float) - CoM[2]) * _binary_image
+#         # Substract center of mass and mask
+#         ZZ = (ZZ.astype(float) - CoM[0]) * _binary_image
+#         YY = (YY.astype(float) - CoM[1]) * _binary_image
+#         XX = (XX.astype(float) - CoM[2]) * _binary_image
 
-        # Concatenate to single (Nx3) coordinate vector
-        ZYX = np.vstack([ZZ.ravel(), YY.ravel(), XX.ravel()]).transpose((1, 0))
+#         # Concatenate to single (Nx3) coordinate vector
+#         ZYX = np.vstack([ZZ.ravel(), YY.ravel(), XX.ravel()]).transpose((1, 0))
 
-        # Calculate orientation matrix
-        S = 1/np.sum(_binary_image) * np.dot(ZYX.conjugate().T, ZYX)
-        D, R = np.linalg.eig(S)
+#         # Calculate orientation matrix
+#         S = 1/np.sum(_binary_image) * np.dot(ZYX.conjugate().T, ZYX)
+#         D, R = np.linalg.eig(S)
 
-        # Now create points on the surface of an ellipse
-        props = pd.DataFrame(measure.regionprops_table(_binary_image, properties=(['major_axis_length', 'minor_axis_length'])))
-        semiAxesLengths = [props.loc[0].major_axis_length/2,
-                           props.loc[0].minor_axis_length/2,
-                           props.loc[0].minor_axis_length/2]
-        _pts = fibonacci_sphere(semiAxesLengths, R.T, CoM, samples=n_samples)
-        pts.append(vedo.pointcloud.Points(_pts))
+#         # Now create points on the surface of an ellipse
+#         props = pd.DataFrame(measure.regionprops_table(_binary_image, properties=(['major_axis_length', 'minor_axis_length'])))
+#         semiAxesLengths = [props.loc[0].major_axis_length/2,
+#                            props.loc[0].minor_axis_length/2,
+#                            props.loc[0].minor_axis_length/2]
+#         _pts = fibonacci_sphere(semiAxesLengths, R.T, CoM, samples=n_samples)
+#         pts.append(vedo.pointcloud.Points(_pts))
 
-    return pts
+#     return pts
 
 # def fit_curvature():
 #     """
