740ac804b063a4f3733bd6033a59ea65b1c1b349
Johannes Müller <38459088+jo-mueller@users.noreply.github.com>
2022-05-09 15:15:00 +0200

    made converter helper functions private

--- src/napari_stress/__init__.py
+++ src/napari_stress/__init__.py
@@ -8,7 +8,6 @@ from ._surface import surface_from_label,\
     smooth_sinc,\
     smoothMLS2D,\
     reconstruct_surface
-from ._utils import list_of_points_to_pointsdata, list_of_surfaces_to_surface, list_of_points_to_points, points_to_list_of_points
 from napari_plugin_engine import napari_hook_implementation
 
 @napari_hook_implementation
