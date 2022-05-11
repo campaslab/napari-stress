3c0c52774524522050d7ca5d9d9acacc8743a70e
Johannes Müller <38459088+jo-mueller@users.noreply.github.com>
2022-05-11 13:57:47 +0200

    Renamed function and added docstring

--- src/napari_stress/_preprocess.py
+++ src/napari_stress/_preprocess.py
@@ -7,8 +7,22 @@ from ._utils.time_slicer import frame_by_frame
 
 
 @frame_by_frame
-def resample(image: ImageData,
+def rescale(image: ImageData,
             scale_factors: np.ndarray) -> ImageData:
-    """ Resample an image according to passed scale factors"""
+    """
+    Rescale an image by a given set of scale factors
+
+    Parameters
+    ----------
+    image : ImageData
+    scale_factors : np.ndarray
+        Factors by which image should be scaled. The length of the passed scale
+        factors should match the image dimensions. For instance, rescaling a
+        `[100x100x100]` image requires a `scale_factors` vector of length 3.
+
+    Returns
+    -------
+    ImageData
+    """
 
     return transform.rescale(image, scale=scale_factors)
