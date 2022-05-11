f41e72bc760ab4133d84f5c7bb3488b0ad7b42a8
Johannes Müller <38459088+jo-mueller@users.noreply.github.com>
2022-05-11 13:54:45 +0200

    Made single rescaling function out of redundant implementations

--- src/napari_stress/_preprocess.py
+++ src/napari_stress/_preprocess.py
@@ -1,70 +1,14 @@
-from napari_tools_menu import register_function
 from napari.types import ImageData
 
-from skimage.transform import rescale
-from skimage import filters, measure
+from skimage import transform
 
-import pandas as pd
 import numpy as np
-import vedo
+from ._utils.time_slicer import frame_by_frame
 
 
-def reshape(image: np.ndarray,
-            vsx: float,
-            vsy: float,
-            vsz: float,
-            res_mode: str = 'high'):
-    """
-    Preprocesses an input 3D image for further processing. Preprocessing includes
-    resampling to isotropic voxels.
-
-    Parameters
-    ----------
-    image : ndarray
-        3/4D image array with intensity values in each pixel
-    vsx : float, optional
-        pixel size in x-dimension. Default value: vsx = 2.076
-    vsx : float, optional
-        pixel size in x-dimension. Default value: vsx = 2.076
-    vsx : float, optional
-        pixel size in x-dimension. Default value: vsx = 3.998
-
-    Returns
-    -------
-    image : MxNxK array
-        binary image
-
-    """
-    image_resampled = []
-
-    # resample every timepoint
-    for t in range(image.shape[0]):
-        _image_resampled = resample(image[t], vsx=vsx, vsy=vsy, vsz=vsz)
-        image_resampled.append(_image_resampled)
-
-    image_resampled = np.asarray(image_resampled)
-
-    return image_resampled
-
-
-@register_function(menu="Process > Resample image (skimage, ns)")
+@frame_by_frame
 def resample(image: ImageData,
-             vsz: float,
-             vsy: float,
-             vsx: float,
-             res_mode='high') -> ImageData:
-    """
-    Resample an image with anistropic voxels of size vsx, vsy and vsz to isotropic
-    voxel size of smallest or largest resolution
-    """
-    # choose final voxel size
-    if res_mode == 'high':
-        vs = np.min([vsx, vsy, vsz])
+            scale_factors: np.ndarray) -> ImageData:
+    """ Resample an image according to passed scale factors"""
 
-    elif res_mode == 'low':
-        vs = np.max([vsx, vsy, vsz])
-
-    factor = np.asarray([vsz, vsy, vsx])/vs
-    image_rescaled = rescale(image, factor, anti_aliasing=True)
-
-    return image_rescaled
+    return transform.rescale(image, scale=scale_factors)
