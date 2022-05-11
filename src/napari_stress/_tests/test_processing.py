1a3a19f1ba76fcd602614e5c7ef127a4b576c331
Johannes Müller <38459088+jo-mueller@users.noreply.github.com>
2022-05-11 14:00:07 +0200

    Renamed rescaling function in tests

--- src/napari_stress/_tests/test_processing.py
+++ src/napari_stress/_tests/test_processing.py
@@ -1,18 +1,18 @@
 # -*- coding: utf-8 -*-
 import numpy as np
-from napari_stress import resample
+from napari_stress import rescale
 
-def test_resampling():
+def test_rescaling():
 
     image = np.random.random(size=(2, 100, 100, 100))
 
-    resampled_image = resample(image, scale_factors = np.asarray([1, 1, 1]))
+    resampled_image = rescale(image, scale_factors = np.asarray([1, 1, 1]))
     assert np.array_equal(resampled_image.shape, image.shape)
 
-    resampled_image = resample(image, scale_factors = np.asarray([0.5, 0.5, 0.5]))
+    resampled_image = rescale(image, scale_factors = np.asarray([0.5, 0.5, 0.5]))
     assert np.array_equal(
         resampled_image.shape[1:], (np.asarray(image.shape)/2)[1:]
         )
 
 if __name__ == '_main__':
-    test_resampling()
+    test_rescaling()
