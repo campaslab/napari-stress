e710fdd6a816d98264b8c9c11e29f6a80bfbc23b
Johannes Müller <38459088+jo-mueller@users.noreply.github.com>
2022-05-11 15:07:29 +0200

    Added test for 3D data

--- src/napari_stress/_tests/test_processing.py
+++ src/napari_stress/_tests/test_processing.py
@@ -2,7 +2,7 @@
 import numpy as np
 from napari_stress import rescale
 
-def test_rescaling():
+def test_rescaling4d():
 
     image = np.random.random(size=(2, 100, 100, 100))
 
@@ -14,5 +14,7 @@ def test_rescaling():
         resampled_image.shape[1:], (np.asarray(image.shape)/2)[1:]
         )
 
-if __name__ == '_main__':
-    test_rescaling()
+def test_rescaling3D():
+    image = np.random.random(size=(100, 100, 100))
+    resampled_image = rescale(image, scale_factors = np.asarray([1, 1, 1]))
+    assert np.array_equal(resampled_image.squeeze().shape, image.squeeze().shape)
