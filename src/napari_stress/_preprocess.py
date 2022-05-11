bb007de5a31c1b9a0e864591c549f77e6291bc3a
Johannes Müller <38459088+jo-mueller@users.noreply.github.com>
2022-05-11 15:29:28 +0200

    Restructured rescale functoin to take rescale factors as individual arguments per axis

--- src/napari_stress/_preprocess.py
+++ src/napari_stress/_preprocess.py
@@ -8,21 +8,34 @@ from ._utils.time_slicer import frame_by_frame
 
 @frame_by_frame
 def rescale(image: ImageData,
-            scale_factors: np.ndarray) -> ImageData:
+            dimension_1: float = 1.0,
+            dimension_2: float = 1.0,
+            dimension_3: float = 1.0) -> ImageData:
     """
-    Rescale an image by a given set of scale factors
+    Rescale an image by a given set of scale factors.
+
+    If the image is a 2D image, only the first two parameters
+    (`dimension_1` and `dimension_2`) will be used.
 
     Parameters
     ----------
     image : ImageData
-    scale_factors : np.ndarray
-        Factors by which image should be scaled. The length of the passed scale
-        factors should match the image dimensions. For instance, rescaling a
-        `[100x100x100]` image requires a `scale_factors` vector of length 3.
+    dimension_1 : float, optional
+        factor by which to scale the image along the first dimension. The default is 1.0.
+    dimension_2 : float, optional
+        factor by which to scale the image along the second dimension. The default is 1.0.
+    dimension_3 : float, optional
+        factor by which to scale the image along the third dimension. The default is 1.0.
 
     Returns
     -------
     ImageData
+
     """
+
+    scale_factors = np.asarray([dimension_1, dimension_2, dimension_3])
+
+    # Make sure to use the correct amount of factors if dimension of image is smaller than three
+    scale_factors = scale_factors[:len(image.shape)]
 
     return transform.rescale(image, scale=scale_factors)
