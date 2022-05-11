9d6d6ab4e171878c189a647e036d1ba8bfd83e20
Johannes Müller <38459088+jo-mueller@users.noreply.github.com>
2022-05-11 15:33:19 +0200

    adjust tests

--- src/napari_stress/_tests/test_processing.py
+++ src/napari_stress/_tests/test_processing.py
@@ -6,15 +6,24 @@ def test_rescaling4d():
 
     image = np.random.random(size=(2, 100, 100, 100))
 
-    resampled_image = rescale(image, scale_factors = np.asarray([1, 1, 1]))
+    resampled_image = rescale(image,
+                              dimension_1 = 1.0,
+                              dimension_2 = 1.0,
+                              dimension_3 = 1.0)
     assert np.array_equal(resampled_image.shape, image.shape)
 
-    resampled_image = rescale(image, scale_factors = np.asarray([0.5, 0.5, 0.5]))
+    resampled_image = rescale(image,
+                              dimension_1 = 0.5,
+                              dimension_2 = 0.5,
+                              dimension_3 = 0.5)
     assert np.array_equal(
         resampled_image.shape[1:], (np.asarray(image.shape)/2)[1:]
         )
 
 def test_rescaling3D():
     image = np.random.random(size=(100, 100, 100))
-    resampled_image = rescale(image, scale_factors = np.asarray([1, 1, 1]))
+    resampled_image = rescale(image,
+                              dimension_1 = 1.0,
+                              dimension_2 = 1.0,
+                              dimension_3 = 1.0)
     assert np.array_equal(resampled_image.squeeze().shape, image.squeeze().shape)
