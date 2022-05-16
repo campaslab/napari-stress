a8ea9c5551e72d48e83e0e3d4e744ac4830ef57d
Johannes Müller <38459088+jo-mueller@users.noreply.github.com>
2022-05-16 17:08:36 +0200

    added conversion function for return type `List[LayerDataTuple]`

--- src/napari_stress/_utils/time_slicer.py
+++ src/napari_stress/_utils/time_slicer.py
@@ -71,7 +71,8 @@ class TimelapseConverter:
             SurfaceData: self._list_of_surfaces_to_surface,
             ImageData: self._list_of_images_to_image,
             LabelsData: self._list_of_images_to_image,
-            LayerDataTuple: self._list_of_layerdatatuple_to_layerdatatuple
+            LayerDataTuple: self._list_of_layerdatatuple_to_layerdatatuple,
+            List[LayerDataTuple]: self._list_of_multiple_ldtuples_to_multiple_ldt_tuples
             }
 
         # This list of aliases allows to map LayerDataTuples to the correct napari.types
@@ -136,6 +137,28 @@ class TimelapseConverter:
             raise TypeError(f'{layertype} list to data conversion currently not supported.')
         conversion_function = self.funcs_list_to_data[layertype]
         return conversion_function(data)
+
+    # =============================================================================
+    # LayerDataTuple(s)
+    # =============================================================================
+
+    def _list_of_multiple_ldtuples_to_multiple_ldt_tuples(self,
+                                                          tuple_data: list,
+                                                          ) -> List[LayerDataTuple]:
+        """If a function returns a list of LayerDataTuple"""
+
+        # Convert data to array with dimensions [frame, results, data]
+        data = np.asarray(tuple_data)
+        layertypes = data[:, -1, -1]
+
+        converted_tuples = []
+        for idx, res_type in enumerate(layertypes):
+            tuples_to_convert = data[:, idx]
+            converted_tuples.append(
+                self._list_of_layerdatatuple_to_layerdatatuple(list(tuples_to_convert))
+                )
+
+        return converted_tuples
 
 
     def _list_of_layerdatatuple_to_layerdatatuple(self,
