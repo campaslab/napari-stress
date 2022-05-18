941193a839186736f1442e4a4c5f2bd126772207
Johannes Müller <38459088+jo-mueller@users.noreply.github.com>
2022-05-18 13:37:34 +0200

    Added tests for sh expansion

---
+++ src/napari_stress/_tests/test_surface.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+
+def test_spherical_harmonics():
+    import napari_stress
+    import vedo
+
+    ellipse = vedo.shapes.Ellipsoid()
+    points = napari_stress.fit_spherical_harmonics(ellipse.points(), max_degree=3)
+
+    assert np.array_equal(ellipse.points().shape, points.shape)
