652ed8db60a29b568df72ec7ab1cfeced8465480
Johannes Müller <38459088+jo-mueller@users.noreply.github.com>
2022-05-19 11:04:45 +0200

    typo
    Added docstrings to utility functions

--- src/napari_stress/_utils/fit_utils.py
+++ src/napari_stress/_utils/fit_utils.py
@@ -2,19 +2,35 @@ import numpy as np
 import inspect
 
 def _sigmoid(x, center, amplitude, slope, offset):
-    "https://stackoverflow.com/questions/55725139/fit-sigmoid-function-s-shape-curve-to-data-using-python"
+    """
+    Sigmoidal fit function
+    https://stackoverflow.com/questions/55725139/fit-sigmoid-function-s-shape-curve-to-data-using-python
+    """
     return amplitude / (1 + np.exp(-slope*(x-center))) + offset
 
 def _gaussian(x, center, sigma, amplitude):
+    """
+    Gaussian normal fit function
+    https://en.wikipedia.org/wiki/Normal_distribution
+    """
     return amplitude/np.sqrt((2*np.pi*sigma**2)) * np.exp(-(x - center)**2 / (2*sigma**2))
 
 def _detect_maxima(profile, center: float = None):
+    """
+    Simple function to find the maximum's index of data with a single peak.
+    """
     return np.argmax(profile)
 
 def _detect_drop(profile, center: float = None):
+    """
+    Simple function to find the location of the steepest drop for sigmoidal data
+    """
     return np.argmax(np.abs(np.diff(profile)))
 
 def _func_args_to_list(func: callable) -> list:
+    """
+    Simple function to return a functions' keywords as a list of strings
+    """
 
     sig = inspect.signature(func)
     return list(sig.parameters.keys())
