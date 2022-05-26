3f1207b41195bd27b2ddb16a1adb7cd10f75238e
Johannes Müller <38459088+jo-mueller@users.noreply.github.com>
2022-05-26 11:50:10 +0200

    refactored variables

--- src/napari_stress/_spherical_harmonics/_sh_implementations.py
+++ src/napari_stress/_spherical_harmonics/_sh_implementations.py
@@ -4,15 +4,16 @@ import vedo
 import numpy as np
 
 import pyshtools
+from . import sph_func_SPB as sph_f
 
-import sph_func_SPB as sph_f
+from typing import Tuple
 
 from ._utils import Conv_3D_pts_to_Elliptical_Coors,\
     Least_Squares_Harmonic_Fit
 
 def shtools_spherical_harmonics_expansion(points: PointsData,
                                           max_degree: int = 5
-                                          ) -> PointsData:
+                                          ) -> Tuple[PointsData, np.ndarray]:
     """
     Approximate a surface by spherical harmonics expansion with pyshtools implementation.
 
@@ -36,8 +37,8 @@ def shtools_spherical_harmonics_expansion(points: PointsData,
                                             relative_coordinates[:, 1],
                                             relative_coordinates[:, 2])
     radius = spherical_coordinates[0]
-    latitude = np.rad2deg(spherical_coordinates[1])
-    longitude = np.rad2deg(spherical_coordinates[2])
+    longitude = np.rad2deg(spherical_coordinates[1])
+    latitude = np.rad2deg(spherical_coordinates[2])
 
     # Find spherical harmonics expansion coefficients until specified degree
     opt_fit_params = pyshtools._SHTOOLS.SHExpandLSQ(radius, latitude, longitude,
@@ -48,13 +49,14 @@ def shtools_spherical_harmonics_expansion(points: PointsData,
 
     # Convert points back to cartesian coordinates
     points = vedo.spher2cart(values,
-                             np.deg2rad(latitude),
-                             np.deg2rad(longitude))
+                             spherical_coordinates[1],
+                             spherical_coordinates[2]).transpose()
 
-    return points.transpose() + center[np.newaxis, :]
+    points = points + center[np.newaxis, :]
+    return points, spherical_harmonics_coeffcients.to_array()
 
 def stress_spherical_harmonics_expansion(points: PointsData,
-                                         max_degree: int = 5) -> PointsData:
+                                         max_degree: int = 5) -> Tuple[PointsData, np.ndarray]:
     """
     Approximate a surface by spherical harmonics expansion with stress implementation.
 
@@ -72,6 +74,8 @@ def stress_spherical_harmonics_expansion(points: PointsData,
     # get LS Ellipsoid estimate and get point cordinates in elliptical coordinates
     points_elliptical_1, points_elliptical_2 = Conv_3D_pts_to_Elliptical_Coors(points)
 
+    # This implementation fits a superposition of three sets of spherical harmonics
+    # to the data, one for each cardinal direction (x/y/z).
     optimal_fit_parameters = Least_Squares_Harmonic_Fit(
         fit_degree=max_degree,
         points_ellipse_coords = (points_elliptical_1, points_elliptical_2),
@@ -81,10 +85,12 @@ def stress_spherical_harmonics_expansion(points: PointsData,
     Y_fit_sph_coef_mat = sph_f.Un_Flatten_Coef_Vec(optimal_fit_parameters[:, 1], max_degree)
     Z_fit_sph_coef_mat = sph_f.Un_Flatten_Coef_Vec(optimal_fit_parameters[:, 2], max_degree)
 
+    spherical_harmonics_coeffcients = np.stack([X_fit_sph_coef_mat, Y_fit_sph_coef_mat, Z_fit_sph_coef_mat])
+
     # Create SPH_func to represent X, Y, Z:
-    X_fit_sph = sph_f.sph_func(X_fit_sph_coef_mat, max_degree)
-    Y_fit_sph = sph_f.sph_func(Y_fit_sph_coef_mat, max_degree)
-    Z_fit_sph = sph_f.sph_func(Z_fit_sph_coef_mat, max_degree)
+    X_fit_sph = sph_f.sph_func(spherical_harmonics_coeffcients[0], max_degree)
+    Y_fit_sph = sph_f.sph_func(spherical_harmonics_coeffcients[1], max_degree)
+    Z_fit_sph = sph_f.sph_func(spherical_harmonics_coeffcients[2], max_degree)
 
     X_fit_sph_UV_pts = X_fit_sph.Eval_SPH(points_elliptical_1, points_elliptical_2)
     Y_fit_sph_UV_pts = Y_fit_sph.Eval_SPH(points_elliptical_1, points_elliptical_2)
@@ -100,4 +106,4 @@ def stress_spherical_harmonics_expansion(points: PointsData,
     mean curvatures very easily.
     """
 
-    return fitted_points
+    return fitted_points, spherical_harmonics_coeffcients
