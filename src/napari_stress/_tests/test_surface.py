5581fe2e0bd1f2211ce4981a47c299930fbf0e14
Johannes Müller <38459088+jo-mueller@users.noreply.github.com>
2022-05-26 15:30:07 +0200

    added test for surface reconstruction

--- src/napari_stress/_tests/test_surface.py
+++ src/napari_stress/_tests/test_surface.py
@@ -1,11 +1,21 @@
 # -*- coding: utf-8 -*-
 import numpy as np
+import napari_stress
 
 def test_spherical_harmonics():
-    import napari_stress
+
     import vedo
 
     ellipse = vedo.shapes.Ellipsoid()
     points = napari_stress.fit_spherical_harmonics(ellipse.points(), max_degree=3)
 
     assert np.array_equal(ellipse.points().shape, points[:, 1:].shape)
+
+def test_reconstruction():
+    import vedo
+    points = vedo.shapes.Ellipsoid().points() * 100
+
+    surface = napari_stress.reconstruct_surface(points)
+
+if __name__ == '__main__':
+    test_reconstruction()
