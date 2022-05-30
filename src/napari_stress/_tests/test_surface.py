0dd9fd6d46edb3c21889c90768c853d58ceef60c
Johannes Müller <38459088+jo-mueller@users.noreply.github.com>
2022-05-30 11:42:33 +0200

    Moved test to surface test file

--- src/napari_stress/_tests/test_surface.py
+++ src/napari_stress/_tests/test_surface.py
@@ -9,3 +9,16 @@ def test_spherical_harmonics():
     points = napari_stress.fit_spherical_harmonics(ellipse.points(), max_degree=3)
 
     assert np.array_equal(ellipse.points().shape, points[:, 1:].shape)
+
+def test_smoothing():
+    from napari_stress import smoothMLS2D
+    import vedo
+
+    points = vedo.shapes.Sphere(res=30).points() * 10
+    points += np.random.uniform(size=points.shape)
+
+    smoothed_points = smoothMLS2D(points, factor=0.25, radius=0)
+    smoothed_points = smoothMLS2D(points, factor=1.0, radius=4)
+
+if __name__ == '__main__':
+    test_smoothing()
