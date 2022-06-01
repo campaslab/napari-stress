9794e11320a819b48bc169a52237f7bf9145643c
Johannes Müller <38459088+jo-mueller@users.noreply.github.com>
2022-06-01 18:07:29 +0200

    removed an old function from init

--- src/napari_stress/__init__.py
+++ src/napari_stress/__init__.py
@@ -2,8 +2,7 @@ __version__ = "0.0.5"
 
 from ._refine_surfaces import trace_refinement_of_surface
 from ._preprocess import rescale
-from ._surface import surface_from_label,\
-    adjust_surface_density,\
+from ._surface import  adjust_surface_density,\
     smooth_sinc,\
     smoothMLS2D,\
     reconstruct_surface,\
