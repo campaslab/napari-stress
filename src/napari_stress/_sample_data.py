9452f75b7710e3754f2e726a3611b079ce2c6cc9
Robert Haase <haesleinhuepf@users.noreply.github.com>
2022-06-06 21:32:26 +0200

    fix typo

--- src/napari_stress/_sample_data.py
+++ src/napari_stress/_sample_data.py
@@ -7,7 +7,7 @@ from pathlib import Path
 
 DATA_ROOT  = os.path.join(Path(__file__).parent, 'sample_data')
 
-def get_dropplet_point_cloud() -> LayerDataTuple:
+def get_droplet_point_cloud() -> LayerDataTuple:
     """Generates a sample point cloud of a droplet surface"""
     
     df = pd.read_csv(os.path.join(DATA_ROOT, 'dropplet_point_cloud.csv'), sep=',')
