b4ed84fc61e613787948a5ce3b4bfada2d94aa51
Johannes Müller <38459088+jo-mueller@users.noreply.github.com>
2022-06-22 13:55:11 +0200

    removed viewer from testing

--- src/napari_stress/_tests/test_surface.py
+++ src/napari_stress/_tests/test_surface.py
@@ -19,16 +19,10 @@ def test_ellipsoid_points():
     pointcloud = np.random.normal(size=(1000, 3)) * 10 * np.array([1,2,3])[None, :]
     ellipse_points = napari_stress.fit_ellipsoid_to_pointcloud_points(pointcloud, pvalue=0.5)
     axis = napari_stress.fit_ellipsoid_to_pointcloud_vectors(pointcloud, pvalue=0.5)
-    
-    import napari
-    viewer = napari.Viewer()
-    viewer.add_points(pointcloud, size=0.5, face_color='cyan')
-    viewer.add_points(ellipse_points, size=0.5, face_color='orange')
-    viewer.add_vectors(axis)
-    
+    axis = napari_stress.fit_ellipsoid_to_pointcloud_vectors(pointcloud, pvalue=0.5, normalize=True)
+
     pointcloud = vedo.shapes.Ellipsoid().points() * 10
     ellipse_points = napari_stress.fit_ellipsoid_to_pointcloud_points(pointcloud, pvalue=0.5)
-    
+
 if __name__ == '__main__':
     test_ellipsoid_points()
-
