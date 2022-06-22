ac61b1eabaefaf73aa282094f9919a18e3b6dbe1
Johannes Müller <38459088+jo-mueller@users.noreply.github.com>
2022-06-22 15:55:48 +0200

    test 4d vector handling

--- src/napari_stress/_tests/test_surface.py
+++ src/napari_stress/_tests/test_surface.py
@@ -24,5 +24,17 @@ def test_ellipsoid_points():
     pointcloud = vedo.shapes.Ellipsoid().points() * 10
     ellipse_points = napari_stress.fit_ellipsoid_to_pointcloud_points(pointcloud, pvalue=0.5)
 
+    # test 4d handling
+    Converter = napari_stress.TimelapseConverter()
+    pointcloud_4d = Converter.list_of_data_to_data([pointcloud, pointcloud + 1], napari.types.PointsData)
+    vectors_4d = napari_stress.fit_ellipsoid_to_pointcloud_vectors(pointcloud_4d)
+
+    # directions_4d = np.zeros_like(pointcloud_4d)
+    # directions_4d[:, 0] = pointcloud_4d[:, 0]
+    # directions_4d[directions_4d[:,0] == 0, 1:] = pointcloud - pointcloud.mean(axis=0)[None, :]
+    # directions_4d[directions_4d[:,0] == 1, 1:] = pointcloud - (pointcloud + 1).mean(axis=0)[None, :]
+
+
+
 if __name__ == '__main__':
     test_ellipsoid_points()
