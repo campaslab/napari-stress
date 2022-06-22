520d5a4ff9b261a3f14a9ce53a9e3ba1ac4a3575
Johannes Müller <38459088+jo-mueller@users.noreply.github.com>
2022-06-22 09:32:48 +0200

    Update test_surface.py

--- src/napari_stress/_tests/test_surface.py
+++ src/napari_stress/_tests/test_surface.py
@@ -15,13 +15,20 @@ def test_surface_to_points():
     surface = (ellipse.points(), np.asarray(ellipse.faces()))
     points = napari_stress.extract_vertex_points(surface)
 
-def test_ellipsoid():
-    pointcloud = vedo.shapes.Ellipsoid().points()
-    ellipse_points = napari_stress.fit_ellipsoid(pointcloud, pvalue=0.5)
-
+def test_ellipsoid_points():
+    pointcloud = np.random.normal(size=(1000, 3)) * 10 * np.array([1,2,3])[None, :]
+    ellipse_points = napari_stress.fit_ellipsoid_to_pointcloud_points(pointcloud, pvalue=0.5)
+    axis = napari_stress.fit_ellipsoid_to_pointcloud_vectors(pointcloud, pvalue=0.5)
+    
+    import napari
     viewer = napari.Viewer()
-    viewer.add_points(pointcloud, size=0.1, face_color='orange')
-    viewer.add_points(ellipse_points, size=0.1, face_color='cyan')
-
+    viewer.add_points(pointcloud, size=0.5, face_color='cyan')
+    viewer.add_points(ellipse_points, size=0.5, face_color='orange')
+    viewer.add_vectors(axis)
+    
+    pointcloud = vedo.shapes.Ellipsoid().points() * 10
+    ellipse_points = napari_stress.fit_ellipsoid_to_pointcloud_points(pointcloud, pvalue=0.5)
+    
 if __name__ == '__main__':
-    test_ellipsoid()
+    test_ellipsoid_points()
+
