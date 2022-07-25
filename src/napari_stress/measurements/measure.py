cb1e2ab649a7f65faa8ee944e323f7f4323fb842
Johannes Müller <38459088+jo-mueller@users.noreply.github.com>
2022-07-25 23:07:31 +0200

    added compatbility decorator to moderate between napari and measurements

--- src/napari_stress/measurements/measure.py
+++ src/napari_stress/measurements/measure.py
@@ -1,13 +1,42 @@
 # -*- coding: utf-8 -*-
 from functools import wraps
 import inspect
+import napari
+import pandas as pd
 
 def naparify_measurement(function):
 
     @wraps(function)
-    def wrapper(layer, *args, **kwargs):
-        if 'metadata' in kwargs:
-            layer.metadata = kwargs['metadata']
+    def wrapper(*args, **kwargs):
 
-        return layer
+        sig = inspect.signature(function)
+        annotations = [
+            sig.parameters[key].annotation for key in sig.parameters.keys()
+            ]
+
+        # If a layer was passed instead of the necessary parameters:
+        # if napari.layers.Layer in annotations and :
+        if 'viewer' in kwargs:
+            new_args = []
+            metadata = args[0].metadata
+            for annotation in annotations:
+                for key in metadata.keys():
+                    if isinstance(metadata[key], annotation):
+                        new_args.append(metadata[key])
+
+            features, metadata = function(*new_args, **kwargs)
+
+            for key in features.keys():
+                args[0].features[key] = features[key]
+
+            for key in metadata.keys():
+                args[0].metadata[key] = metadata[key]
+
+            return args[0]
+
+        else:
+            return function(*args, **kwargs)
+
+
+        # If a viewer exists...?
     return wrapper
