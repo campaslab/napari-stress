4a0e3ad12a8bb9ef0b3a4fc87de2b955ede6c888
Johannes Müller <38459088+jo-mueller@users.noreply.github.com>
2022-08-26 18:41:06 +0200

    made functions public

--- src/napari_stress/_measurements/__init__.py
+++ src/napari_stress/_measurements/__init__.py
@@ -5,3 +5,4 @@ from .curvature import (calculate_mean_curvature_on_manifold,
                         gauss_bonnet_test)
 from .utils import naparify_measurement
 from .stresses import anisotropic_stress, tissue_stress_tensor
+from .geodesics import geodesic_distance_matrix, geodesic_analysis, correlation_on_surface
