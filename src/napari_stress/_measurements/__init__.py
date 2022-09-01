ab67611ed6ff35b1d086e05ab26610c987c697ec
Johannes Müller <38459088+jo-mueller@users.noreply.github.com>
2022-09-01 16:29:57 +0200

    made `comprehensive_analysis` public

--- src/napari_stress/_measurements/__init__.py
+++ src/napari_stress/_measurements/__init__.py
@@ -7,4 +7,4 @@ from .curvature import (calculate_mean_curvature_on_manifold,
 from .utils import naparify_measurement
 from .stresses import anisotropic_stress, tissue_stress_tensor, maximal_tissue_anisotropy
 from .geodesics import geodesic_distance_matrix, geodesic_analysis, correlation_on_surface
-from .toolbox import stress_analysis_toolbox
+from .toolbox import stress_analysis_toolbox, comprehensive_analysis
