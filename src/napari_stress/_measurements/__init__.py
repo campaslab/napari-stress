98dd4d2b02796bf892f6c68fe6753bcc686c3272
Johannes Müller <38459088+jo-mueller@users.noreply.github.com>
2022-09-22 10:49:06 +0200

    made toolbox public
    Made toolbox functions public

--- src/napari_stress/_measurements/__init__.py
+++ src/napari_stress/_measurements/__init__.py
@@ -6,3 +6,4 @@ from .curvature import (calculate_mean_curvature_on_manifold,
                         gauss_bonnet_test)
 from .utils import naparify_measurement
 from .stresses import anisotropic_stress, tissue_stress_tensor, maximal_tissue_anisotropy
+from .toolbox import stress_analysis_toolbox, comprehensive_analysis
