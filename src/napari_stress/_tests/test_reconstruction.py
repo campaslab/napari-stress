8ca8c94503af243e8af402fcf72cf22fff91eaeb
Johannes Müller <38459088+jo-mueller@users.noreply.github.com>
2022-09-30 17:16:54 +0200

    Update test_reconstruction.py

--- src/napari_stress/_tests/test_reconstruction.py
+++ src/napari_stress/_tests/test_reconstruction.py
@@ -2,10 +2,14 @@
 
 def test_reconstruction(make_napari_viewer):
     from napari_stress import reconstruction
+    import napari_process_points_and_surfaces as nppas
+    from skimage import measure
     viewer = make_napari_viewer()
 
     image = viewer.open(r'C:\Users\johamuel\Desktop\droplet_scaled.tif')
-    binary = viewer.open(r'C:\Users\johamuel\Desktop\droplet_binary.tif')
+    binary = viewer.open(r'C:\Users\johamuel\Desktop\droplet_binary.tif')[0]
+    surface = nppas.largest_label_to_surface(binary.data)
+    viewer.add_surface(surface)
 
     widget = reconstruction.droplet_reconstruction_toolbox(viewer)
     viewer.window.add_dock_widget(widget)
