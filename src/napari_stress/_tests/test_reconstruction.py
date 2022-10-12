57e193d3370bd77b44cb46c0906c216d087d409f
Johannes Müller <38459088+jo-mueller@users.noreply.github.com>
2022-10-13 01:19:25 +0200

    used shipped data for test

--- src/napari_stress/_tests/test_reconstruction.py
+++ src/napari_stress/_tests/test_reconstruction.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 def test_reconstruction(make_napari_viewer):
-    from napari_stress import reconstruction
+    from napari_stress import reconstruction, get_droplet_4d
     import napari_process_points_and_surfaces as nppas
     from skimage import measure
     from napari.layers import Layer
@@ -10,23 +10,19 @@ def test_reconstruction(make_napari_viewer):
 
     viewer = make_napari_viewer()
 
-    image = viewer.open(r'C:\Users\johan\Downloads\1_Drop.tif')[0].data[:5]
+
+    image = get_droplet_4d()[0][0]
+    viewer.add_image(image)
 
     widget = reconstruction.droplet_reconstruction_toolbox(viewer)
     viewer.window.add_dock_widget(widget)
 
-    #widget._run()
-
     results = reconstruction.reconstruct_droplet(
         image,
-        voxelsize=np.asarray([2, 1, 1]),
-        target_voxelsize=2
+        voxelsize=np.asarray([1.93, 1, 1]),
+        target_voxelsize=1
         )
 
     for result in results:
         layer = Layer.create(result[0], result[1], result[2])
         viewer.add_layer(layer)
-
-if __name__ == '__main__':
-    import napari
-    test_reconstruction(napari.Viewer)
