a9718dcf9e87e66b194205bfbfb876a14dd07eaf
Johannes Müller <38459088+jo-mueller@users.noreply.github.com>
2022-11-16 17:42:45 +0100

    added tests for lebedev point functions

--- src/napari_stress/_tests/test_spherical_harmonic_fit.py
+++ src/napari_stress/_tests/test_spherical_harmonic_fit.py
@@ -120,6 +120,10 @@ def test_interoperatibility():
     # pysh_z = coeffs_pysh_z.expand(lon = list(np.rad2deg(THETA.flatten())), lat=list(np.rad2deg(PHI.flatten())))
     # pysh_pts = np.stack([pysh_x, pysh_y, pysh_z]).transpose()
 
-if __name__ == '__main__':
-    #import napari
-    test_spherical_harmonics()
+def test_lebedev_points():
+
+    from napari_stress._stress.lebedev_write_SPB import LebFunc
+
+    for i in [6,14,26,38,50,74,86,110,146,170,194, 230,  266,  302, 350,  434,  590,  770, 974, 1202, 1454, 1730, 2030, 2354, 2702, 3074, 3470, 3890, 4334, 4802, 5294, 5810]:
+        print("    %d : [" % i)
+        lf = LebFunc[i]()
