cca2309fd9e33e67920bf1031a2d7df0f36e6b90
Johannes Müller <38459088+jo-mueller@users.noreply.github.com>
2023-09-20 17:33:33 +0200

    made frame by frame and TimelapseConverter public

--- src/napari_stress/_utils/__init__.py
+++ src/napari_stress/_utils/__init__.py
@@ -2,3 +2,4 @@
 
 from .import_export_settings import import_settings, export_settings
 from . _aggregate_measurements import compile_data_from_layers
+from .frame_by_frame import TimelapseConverter, frame_by_frame
