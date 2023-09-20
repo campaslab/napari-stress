ec60e78cb499edc1614ce0de6a58c52e5249f625
Johannes Müller <38459088+jo-mueller@users.noreply.github.com>
2023-09-20 16:14:18 +0200

    code style

--- src/napari_stress/_plotting/__init__.py
+++ src/napari_stress/_plotting/__init__.py
@@ -1,2 +1,3 @@
 from .create_plots import (draw_chronological_lineplot_with_errors,
-                           draw_chronological_kde_plot)
+                           draw_chronological_kde_plot,
+                           create_all_stress_plots)
