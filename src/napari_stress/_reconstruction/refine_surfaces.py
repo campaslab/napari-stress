f0ded4629d1fb12ce59ee715ff339812ba315ea6
Johannes Soltwedel <38459088+jo-mueller@users.noreply.github.com>
2023-10-30 13:13:38 +0100

    moved fit functions to separate file

--- src/napari_stress/_reconstruction/refine_surfaces.py
+++ src/napari_stress/_reconstruction/refine_surfaces.py
@@ -6,7 +6,6 @@ import numpy as np
 import pandas as pd
 from napari.types import ImageData, LayerDataTuple, PointsData
 from napari_tools_menu import register_function
-from scipy.optimize import curve_fit
 
 from .._utils.fit_utils import (
     _detect_max_gradient,
@@ -99,6 +98,8 @@ def trace_refinement_of_surface(
     from .. import _vectors as vectors
     from .._measurements.intensity import sample_intensity_along_vector
     from .._measurements.measurements import distance_to_k_nearest_neighbors
+    from .fit_utils import _mean_squared_error, _identify_outliers, _fancy_edge_fit
+
 
     if isinstance(selected_fit_type, str):
         selected_fit_type = fit_types(selected_fit_type)
@@ -191,13 +192,14 @@ def trace_refinement_of_surface(
             column_names=["fraction_variance_unexplained_log"],
             which=["above"],
             factor=outlier_tolerance,
+            merge='or'
         )
         fit_data = fit_data[good_points]
         intensity_along_vector = intensity_along_vector[good_points]
 
     # remove NaNs from reconstructed points
     no_nan_idx = ~np.isnan(np.stack(fit_data["start_points"].to_numpy()).squeeze()).any(
-        axis=1
+        axis=0
     )
     fit_data = fit_data[no_nan_idx]
     intensity_along_vector = intensity_along_vector[no_nan_idx]
@@ -251,150 +253,81 @@ def trace_refinement_of_surface(
     return (layer_points, layer_normals)
 
 
-def _identify_outliers(
-    table: pd.DataFrame,
-    column_names: list,
-    which: list,
-    factor: float = 1.5,
-    merge: float = "and",
-) -> np.ndarray:
+@register_function(menu="Points > Resample spherical pointcloud (n-STRESS)")
+def resample_pointcloud(
+        points: 'napari.types.PointsData',
+        sampling_length: float = 5
+        ) -> 'napari.types.PointsData':
     """
-    Identify outliers in a table based on the IQR method.
+    Resampe a spherical-like pointcloud on fibonacci grid.
 
     Parameters
     ----------
-    table : pd.DataFrame
-        Table containing the data.
-    column_names : list
-        List of column names to check for outliers.
-    which : list
-        List of strings indicating which outliers to identify. Options are
-        'above', 'below', and 'both'.
-    factor : float, optional
-        Factor to multiply the IQR with. The default is 1.5.
-    merge : float, optional
-        Merge outliers identified in different columns. Options are 'and' and
-        'or'. The default is 'and'.
-    """
-    if isinstance(table, dict):
-        table = pd.DataFrame(table)
-
-    # True if values are good, False if outliers
-    indices = np.ones((len(column_names), len(table)), dtype=bool)
-    indices[:, table[table.isnull().any(axis=1)].index] = False
-
-    for idx, column in enumerate(column_names):
-        Q1 = table[column].quantile(0.25)
-        Q3 = table[column].quantile(0.75)
-        IQR = Q3 - Q1
-        if which[idx] == "above":
-            idx_outlier = table[column] > (Q3 + factor * IQR)
-        elif which[idx] == "below":
-            idx_outlier = table[column] < (Q1 - factor * IQR)
-        elif which[idx] == "both":
-            idx_outlier = (table[column] < (Q1 - factor * IQR)) + (
-                table[column] > (Q3 + factor * IQR)
-            )
-        indices[idx, table[idx_outlier].index] = False
-
-    if merge == "and":
-        indices = np.all(indices, axis=0)
-    elif merge == "or":
-        indices = ~np.any(~indices, axis=0)
-
-    return indices
-
-
-def _fancy_edge_fit(
-    array: np.ndarray, selected_edge_func: edge_functions = edge_functions.interior
-) -> float:
-    """
-    Fit a line profile with a gaussian normal curve or a sigmoidal function.
-
-    https://docs.scipy.org/doc/scipy/reference/generated/scipy.optimize.curve_fit.html
-
-    Parameters
-    ----------
-    profile : np.ndarray
-        DESCRIPTION.
-    mode : int, optional
-        DESCRIPTION. The default is 0.
+    points : PointsData
+    sampling_length : float, optional
+        Distance between sampled point locations. The default is 5.
 
     Returns
     -------
-    float
-        DESCRIPTION.
+    resampled_points : TYPE
+
     """
-    params = _function_args_to_list(selected_edge_func)[1:]
-    array = [x for x in array if not np.isnan(x)]  # filter out nans
-    try:
-        if selected_edge_func == _sigmoid:
+    from scipy.interpolate import griddata
+    import vedo
+    from .fit_utils import _fibonacci_sampling
 
-            # trim begin of trace to get rid of rising intensity slope
-            ind_max = np.argmax(array)
-            array = array[ind_max:]
+    # convert to spherical, relative coordinates
+    center = np.mean(points, axis=0)
+    points_centered = points - center
+    points_spherical = vedo.cart2spher(
+        points_centered[:, 0], points_centered[:, 1], points_centered[:, 2]
+    ).T
 
-            amplitude_est = max(array)
-            center_est = np.where(np.diff(array) == np.diff(array).min())[0][0]
-            background_slope = 0
-            slope_est = 1
-            offset_est = min(array)
-            parameter_estimate = [
-                center_est,
-                amplitude_est,
-                slope_est,
-                background_slope,
-                offset_est,
-            ]
+    # estimate point number according to passed sampling length
+    mean_radius = points_spherical[:, 0].mean()
+    surface_area = mean_radius**2 * 4 * np.pi
+    n = int(surface_area / sampling_length**2)
 
-            optimal_fit_parameters, _covariance = curve_fit(
-                selected_edge_func, np.arange(len(array)), array, parameter_estimate
-            )
+    # sample points on unit-sphere according to fibonacci-scheme
+    sampled_points = _fibonacci_sampling(n)
+    sampled_points = vedo.utils.cart2spher(
+        sampled_points[:, 0], sampled_points[:, 1], sampled_points[:, 2]
+    ).T
 
-            optimal_fit_parameters[0] = optimal_fit_parameters[0] + ind_max
+    # interpolate cartesian coordinates on (theta, phi) grid
+    theta_interpolation = np.concatenate(
+        [points_spherical[:, 1], points_spherical[:, 1], points_spherical[:, 1]]
+    )
+    phi_interpolation = np.concatenate(
+        [
+            points_spherical[:, 2] + 2 * np.pi,
+            points_spherical[:, 2],
+            points_spherical[:, 2] - 2 * np.pi,
+        ]
+    )
 
-        elif selected_edge_func == _gaussian:
-            parameter_estimate = [len(array) / 2, len(array) / 2, max(array)]
-            optimal_fit_parameters, _covariance = curve_fit(
-                selected_edge_func,
-                np.arange(0, len(array), 1),
-                array,
-                parameter_estimate,
-            )
+    new_x = griddata(
+        np.stack([theta_interpolation, phi_interpolation]).T,
+        list(points_centered[:, 0]) * 3,
+        sampled_points[:, 1:],
+        method="cubic",
+    )
 
-        # retrieve errors from covariance matrix
-        parameter_error = np.sqrt(np.diag(_covariance))
+    new_y = griddata(
+        np.stack([theta_interpolation, phi_interpolation]).T,
+        list(points_centered[:, 1]) * 3,
+        sampled_points[:, 1:],
+        method="cubic",
+    )
 
-    # If fit fails, replace bad values with NaN
-    except Exception:
-        optimal_fit_parameters = np.repeat(np.nan, len(params))
-        parameter_error = np.repeat(np.nan, len(params))
+    new_z = griddata(
+        np.stack([theta_interpolation, phi_interpolation]).T,
+        list(points_centered[:, 2]) * 3,
+        sampled_points[:, 1:],
+        method="cubic",
+    )
 
-    return optimal_fit_parameters, parameter_error
+    resampled_points = np.stack([new_x, new_y, new_z]).T + center
 
-
-def _mean_squared_error(
-    fit_function: callable, x: np.ndarray, y: np.ndarray, fit_params: list
-) -> List[float]:
-    """
-    Calculate error parameters for a given fit functions and the determined parameters.
-
-    Args:
-        fit_function (callable): Used fit function
-        x (np.ndarray): x-values
-        y (np.ndarray): measured, corresponding y-values
-        fit_params (list): determined fit parameters
-
-    Returns:
-        float: mean squared error
-        float: fraction of variance unexplained
-    """
-    is_number = ~np.isnan(y)
-    x = x[is_number].squeeze()  # filter out nans
-    y = y[is_number].squeeze()  # filter out nans
-    y_fit = fit_function(x, *fit_params)
-
-    mean_squared_error = np.mean((y - y_fit) ** 2)
-    fraction_variance_unexplained = mean_squared_error / np.var(y)
-
-    return mean_squared_error, fraction_variance_unexplained
+    no_nan_idx = np.where(~np.isnan(resampled_points[:, 0]))[0]
+    return resampled_points[no_nan_idx, :]
