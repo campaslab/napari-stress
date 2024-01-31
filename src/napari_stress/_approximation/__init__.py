0974e1bcc48f6d9aa4ec0e07b4c5d395902395a2
Johannes Müller <38459088+jo-mueller@users.noreply.github.com>
2024-02-01 00:28:34 +0100

    replaced other usecases with new expander class

--- src/napari_stress/_approximation/__init__.py
+++ src/napari_stress/_approximation/__init__.py
@@ -3,14 +3,20 @@
 from .fit_ellipsoid import (
     least_squares_ellipsoid,
     expand_points_on_ellipse,
-    normals_on_ellipsoid,
+    normals_on_ellipsoid
 )
 
-from .expansion import EllipsoidExpander
+from .expansion import (
+    EllipsoidExpander,
+    fit_ellipsoid_to_pointcloud,
+    expand_points_on_fitted_ellipsoid,
+)
 
 __all__ = [
     "least_squares_ellipsoid",
     "expand_points_on_ellipse",
     "normals_on_ellipsoid",
+    "fit_ellipsoid_to_pointcloud",
+    "expand_points_on_fitted_ellipsoid",
     "EllipsoidExpander",
 ]
