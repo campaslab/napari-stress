1f9f247bbae44e628059a32eab090c2af1420234
Johannes Müller <38459088+jo-mueller@users.noreply.github.com>
2024-05-14 22:48:34 +0200

    added lsq harmonics fit as member function

--- src/napari_stress/_approximation/expansion.py
+++ src/napari_stress/_approximation/expansion.py
@@ -162,6 +162,46 @@ class SphericalHarmonicsExpander(Expander):
 
         self.properties["power_spectrum"] = power_spectrum
         return power_spectrum
+    
+    def _least_squares_harmonic_fit(
+        self, fit_degree: int, sample_locations: tuple, values: np.ndarray
+    ) -> np.ndarray:
+        """
+        Perform least squares harmonic fit on input points.
+
+        Parameters
+        ----------
+
+        fit_degree: int
+        sample_locations: tuple
+            Input points in elliptical coordinates - required least squares
+            fit to find ellipsoid major/minor axes
+        values: np.ndarray
+            Values to be expanded on the surface. Can be cartesian point coordinates
+            (x/y/z) or radii for a radial expansion.
+
+        Returns
+        -------
+        coefficients: np.ndarray
+            Numpy array holding spherical harmonics expansion coefficients. The size
+            on the type of expansion (cartesian or radial).
+        """
+        from .._stress import lebedev_info_SPB as lebedev_info
+
+        U, V = sample_locations[0], sample_locations[1]
+
+        All_Y_mn_pt_in = []
+
+        for n in range(fit_degree + 1):
+            for m in range(-1 * n, n + 1):
+                Y_mn_coors_in = []
+                Y_mn_coors_in = lebedev_info.Eval_SPH_Basis(m, n, U, V)
+                All_Y_mn_pt_in.append(Y_mn_coors_in)
+        All_Y_mn_pt_in_mat = np.hstack((All_Y_mn_pt_in))
+
+        coefficients = np.linalg.lstsq(All_Y_mn_pt_in_mat, values)[0]
+        return coefficients
+
 
     def _calculate_power_spectrum_individual(self, coefficients, normalize=True):
         power_spectrum = np.zeros(coefficients.shape[0])
