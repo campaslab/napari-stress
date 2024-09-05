26b540aab733703e5409837e52c89a23d9c74db2
Johannes Müller <38459088+jo-mueller@users.noreply.github.com>
2024-09-05 13:04:24 +0200

    fixed test condition for sample data

--- src/napari_stress/_tests/test_sample_data.py
+++ src/napari_stress/_tests/test_sample_data.py
@@ -11,6 +11,6 @@ def test_dropplet_point_cloud():
     ellipsoid = sample_data.make_blurry_ellipsoid()[0]
 
     assert np.array_equal(data[0].shape, (1024, 4))
-    assert data_4d[0].shape[0] == 26704
+    assert data_4d[0].shape[0] == 16628
     assert len(image_data_4d[0]) == 21
     assert np.array_equal(ellipsoid.shape, (64, 64, 64))
