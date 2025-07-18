1e0bd4694655a7cef0a26c6af0abad3a0c1a36e7
Johannes Müller <38459088+jo-mueller@users.noreply.github.com>
2025-07-18 12:29:34 +0200

    streamline sample data testing

--- src/napari_stress/_tests/test_sample_data.py
+++ src/napari_stress/_tests/test_sample_data.py
@@ -1,14 +1,8 @@
-def test_dropplet_point_cloud():
-    import numpy as np
+def test_dropplet_point_cloud(make_napari_viewer):
+    viewer = make_napari_viewer()
+    sample_keys = ["PC_1", "PC_2", "PC_3"]
 
-    from napari_stress import sample_data
-
-    data = sample_data.get_droplet_point_cloud()[0]
-    data_4d = sample_data.get_droplet_point_cloud_4d()[0]
-    image_data_4d = sample_data.get_droplet_4d()[0]
-    ellipsoid = sample_data.make_blurry_ellipsoid()[0]
-
-    assert np.array_equal(data[0].shape, (1024, 4))
-    assert data_4d[0].shape[0] == 16628
-    assert len(image_data_4d[0]) == 21
-    assert np.array_equal(ellipsoid.shape, (64, 64, 64))
+    for key in sample_keys:
+        viewer.layers.clear()
+        viewer.open_sample("napari-stress", key)
+        assert len(viewer.layers) == 1
